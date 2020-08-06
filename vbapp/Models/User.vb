44dd701676de92e74f1511cb8f49e775480c434d
CohenINT <moshe22cohen@gmail.com>
2020-08-06 19:10:45 +0300

    added post get pratctice with return json and string

---
+++ vbapp/Models/User.vb
@@ -0,0 +1,28 @@
+﻿Namespace Models
+    Public Class User
+      
+
+        Property Username() As String
+        Property Age() As String
+        Property Location  () As String
+        Property Language() As String
+
+
+        Public Sub New ()
+            Username=""
+            Location=""
+            Age=0
+            Language=""
+        End Sub
+
+        Public Sub New (user As String,loc As String,age As String,lang As String)
+            Username=user
+            Location=loc
+            Me.Age=age
+            Language=lang
+        End Sub
+
+
+
+    End Class
+End NameSpace
--- vbapp/Program.vb
+++ vbapp/Program.vb
@@ -1,17 +1,62 @@
 Imports System
+Imports System.IO
+Imports System.Net
+Imports System.Reflection
 Imports System.Threading.Tasks
+Imports Newtonsoft.Json
+Imports Newtonsoft.Json.Linq
+Imports vbapp.Models
 
 Module Program
     Sub Main(args As String())
 
 
+        Dim result As String = GetNumberCalcResult()
+        Dim User As Models.User
+        Console.WriteLine("hoii")
+        Console.WriteLine("back to main method, result is {0}", result)
+            Console.WriteLine("************** testing json stuff now *******************")
+        Dim data As User=GetDataJson()
+
+        
+        Console.WriteLine(data.Location)
+        Console.WriteLine(data.Username)
+
+
+
+
+    End Sub
+
+    Public Function GetDataJson() As User
+
+        Dim user As User
+        Const url As String = "http://localhost:4500/getUser"
+        Dim request As HttpWebRequest
+        Dim response As HttpWebResponse = Nothing
+        Dim reader As StreamReader
+
+        request = DirectCast(WebRequest.Create(url), HttpWebRequest)
+        request.Method="POST"
+        response = DirectCast(request.GetResponse(),HttpWebResponse)
+        reader = New  StreamReader(response.GetResponseStream())
+
+        Dim rawresp As String
+        rawresp = reader.ReadToEnd()
+        user=JsonConvert.DeserializeObject(Of User)(rawresp)
+
+        Return user
+
+    End Function
+
+    Public Function GetNumberCalcResult()
+        Dim resultFinal As String = ""
         'Async function to retrieve data from GET request
         Dim t As Task(Of Task(Of String)) = Task.Factory.StartNew(Async Function() As Task(Of String)
                                                                       Dim webClient As New System.Net.WebClient
                                                                       Console.WriteLine("about to begin")
                                                                       Dim url As Uri = New Uri("http://localhost:4500/str")
                                                                       Dim result As String = Await webClient.DownloadStringTaskAsync(url)
-                                                                      
+
                                                                       Return result
 
 
@@ -23,6 +68,7 @@ Module Program
             Console.WriteLine("t Status :{0}", t.Status)
             If (t.IsCompleted.Equals(True)) Then
                 Console.WriteLine("Data accuired: {0}", t.Result.Result)
+                resultFinal = t.Result.Result
 
 
             End If
@@ -32,16 +78,7 @@ Module Program
 
 
         End Try
-
-        Console.WriteLine("hoii")
-        Console.WriteLine("hoii")
-        Console.WriteLine("hoii")
-        Console.WriteLine("hoii")
-    End Sub
-
-
-    Public Function GetNumberCalcResult(var1 As Int16, var2 As Int16)
-        Return var1 + var2
+        Return resultFinal
     End Function
 
 
